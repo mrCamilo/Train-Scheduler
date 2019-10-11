0c69175ffe20f3bcfee094e49aff386d44efe696
Manuel Camilo <mcamilo@protonmail.com>
2019-10-10 21:50:37 -0400

    .

--- trainApp.js
+++ trainApp.js
@@ -1,5 +1,5 @@
 // Initialize Firebase
-const firebaseConfig = {
+const fireBaseConfig = {
     apiKey: "AIzaSyDcGIF003vq_0dk2gmdB8WkpmcYqgdKAwQ",
     authDomain: "coders-bay-872ba.firebaseapp.com",
     databaseURL: "https://coders-bay-872ba.firebaseio.com",
@@ -14,25 +14,24 @@ firebase.initializeApp(fireBaseConfig);
 // Get a reference to the database service
 var database = firebase.database();
 
-    // Initializing our click count at 0
-    var clickCounter = 0;
+var clickCounter = 0;
 
-    // Functions
-    // ================================================================================
+// Submit button to add train info
+$("#anotherRow").on("click", function(event) {
+  event.preventDefault();
 
-    // On Click
-    $("#click-button").on("click", function() {
+  // inputs
+  var trainName = $("#trainNameInput").val().trim();
+  var destinationName = $("#destinationInput").val().trim();
+  var firstTrainTime = $("#firstTrainTimeInput").val().trim();
+  var frequencyInput = $('#freqInput').val().trim();
 
-      // Add 1 to clickCounter
-      clickCounter++;
+  // object with the inputs
+  var newRow = {
+    name: trainName,
+    destination: destinationName,
+    firstTrain: firstTrainTime,
+    freqency: frequencyInput
+  }
+})
 
-      // **** Store Click Data to Firebase in a JSON property called clickCount *****
-      // **** Note how we are using the Firebase .set() method ****
-      // **** .ref() refers to the path you want to save your data to
-      // **** Since we left .ref() blank, it will save to the root directory
-      database.ref().set({
-        clickCount: clickCounter // set clickCounter (variable in JS) equal to new variable clickCount in firebase database
-      });
-
-      // Now! go to https://fir-click-counter-7cdb9.firebaseio.com/ to see the impact to the DB
-    });
